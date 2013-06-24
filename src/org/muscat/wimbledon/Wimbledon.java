eb85a8c82e4a004a4a4721adbcb643b49a6b7b82
James Muscat <none@none>
2013-06-24 14:47:10 +0100

    Make current score display in bold.

--- src/org/muscat/wimbledon/Wimbledon.java
+++ src/org/muscat/wimbledon/Wimbledon.java
@@ -5,6 +5,9 @@ import java.util.Date;
 public class Wimbledon implements ScoresHandler {
 
   final static String ESC = "\033[";
+  final static String CONSOLE_BOLD = (char) 27 + "[1m";
+  final static String CONSOLE_NORMAL = (char) 27 + "[0m";
+
   public static void main(final String[] args) {
 
     final Wimbledon w = new Wimbledon();
@@ -17,7 +20,7 @@ public class Wimbledon implements ScoresHandler {
     System.out.print(ESC + "2J"); // clear screen
 
     for (final MatchScore match : scores.getMatches()) {
-      System.out.format("%-14s%25s %-7s  %-25s %-25s\n", match.getCourt(), match.getPlayer1(), match.getPointsAsString(), match.getScoresAsString(), match.getPlayer2());
+      System.out.format("%-14s%25s %s%-7s%s  %-25s %-25s\n", match.getCourt(), match.getPlayer1(), CONSOLE_BOLD, match.getPointsAsString(), CONSOLE_NORMAL, match.getScoresAsString(), match.getPlayer2());
     }
 
     System.out.println(new Date());
